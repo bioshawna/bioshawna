73d7dedea5da795834cbf210b91deb0f56f7b8cf
bioshawna <okayshawna@gmail.com>
2025-04-25 22:23:55 -0400

    Additional updates

--- TestApp/TestApp/obj/Debug/net9.0/TestApp.AssemblyInfo.cs
+++ TestApp/TestApp/obj/Debug/net9.0/TestApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a431ad7b34a08895fb5ffc07134ff334f07821cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b566e988fca2cf504851cb6687e060ab055c8f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
